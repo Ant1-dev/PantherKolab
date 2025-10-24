7b11e0e6e0ce353b97d7c529a7d70ee14f3658b7
Jaem Dessources <jaem.developer@gmail.com>
2025-10-24 16:17:49 +0000

    Started refactoring of auth components

---
+++ src/app/(auth)/login/login.style.ts
@@ -0,0 +1,7 @@
+
+export const root = {
+    backgroundImage: "url('/images/login-splash.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+};
